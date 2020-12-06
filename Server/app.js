93e2f88ca0c75b95b8b9c80aef42d0c4843c3c3a
zhang.yizhe@northeastern.edu <zhang.yizhe@northeastern.edu>
2020-12-06 18:19:00 -0500

    change entrance

--- Server/app.js
+++ Server/app.js
@@ -1,50 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var usersRouter = require('./routes/users');
-var bodyParser = require("body-parser");
-var cors = require("cors");
-var app = express();
+// import libs
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const usersRouter = require('./routes/users');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const app = express();
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+app.listen(port, () => console.log(`start at ${port}`));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', usersRouter);
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
 
 // middlewares
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods","*")
-  res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+app.use('/', usersRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../Client')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
 });
 app.use(cors());
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
